c4f8c44f305906a3b31fc382683e2b6a20c1ec9b
Benjamin SIMON <benjamin.simon@catonetworks.com>
2024-09-14 13:48:39 +0200

    migrate static_host to new SDK & removal optimisation

--- internal/provider/resource_static_host.go
+++ internal/provider/resource_static_host.go
@@ -3,12 +3,15 @@ package provider
 import (
 	"context"
 
-	"github.com/BenEkpy/terraform-provider-cato-oss/internal/catogo"
+	"github.com/BenEkpy/terraform-provider-cato-oss/internal/utils"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	cato_models "github.com/routebyintuition/cato-go-sdk/models"
 )
 
 var (
@@ -21,7 +24,7 @@ func NewStaticHostResource() resource.Resource {
 }
 
 type staticHostResource struct {
-	client *catogo.Client
+	client *catoClientData
 }
 
 type StaticHost struct {
@@ -40,42 +43,41 @@ func (r *staticHostResource) Schema(_ context.Context, _ resource.SchemaRequest,
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			"id": schema.StringAttribute{
-				Description: "Identifier for the host",
+				Description: "",
 				Computed:    true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.UseStateForUnknown(),
 				},
 			},
 			"site_id": schema.StringAttribute{
-				Description: "Host Site ID",
+				Description: "",
 				Required:    true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplace(),
 				},
 			},
 			"name": schema.StringAttribute{
-				Description: "Host name",
+				Description: "",
 				Required:    true,
 			},
 			"ip": schema.StringAttribute{
-				Description: "Host IP Address",
+				Description: "",
 				Required:    true,
 			},
 			"mac_address": schema.StringAttribute{
-				Description: "Host Mac Address",
+				Description: "",
 				Optional:    true,
 			},
 		},
 	}
 }
 
-func (d *staticHostResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+func (r *staticHostResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
 	if req.ProviderData == nil {
 		return
 	}
 
-	confData := req.ProviderData.(*catoClientData)
-	d.client = confData.catogo
+	r.client = req.ProviderData.(*catoClientData)
 }
 
 func (r *staticHostResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
@@ -87,28 +89,38 @@ func (r *staticHostResource) Create(ctx context.Context, req resource.CreateRequ
 		return
 	}
 
-	input := catogo.AddStaticHostInput{
+	// setting input
+	input := cato_models.AddStaticHostInput{
 		Name:       plan.Name.ValueString(),
-		Ip:         plan.Ip.ValueString(),
+		IP:         plan.Ip.ValueString(),
 		MacAddress: plan.MacAddress.ValueStringPointer(),
 	}
 
-	body, err := r.client.AddStaticHost(plan.SiteId.ValueString(), input)
+	tflog.Debug(ctx, "static_host create", map[string]interface{}{
+		"input": utils.InterfaceToJSONString(input),
+	})
+
+	body, err := r.client.catosdk.SiteAddStaticHost(ctx, plan.SiteId.ValueString(), input, r.client.AccountId)
 	if err != nil {
 		resp.Diagnostics.AddError(
-			"Cato API error",
+			"Catov2 API error",
 			err.Error(),
 		)
 		return
 	}
-
-	plan.Id = types.StringValue(body.HostId)
 
 	diags = resp.State.Set(ctx, plan)
 	resp.Diagnostics.Append(diags...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
+
+	// overiding state with static host id
+	resp.State.SetAttribute(
+		ctx,
+		path.Empty().AtName("id"),
+		body.Site.GetAddStaticHost().HostID,
+	)
 }
 
 func (r *staticHostResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
@@ -123,16 +135,21 @@ func (r *staticHostResource) Update(ctx context.Context, req resource.UpdateRequ
 		return
 	}
 
-	input := catogo.UpdateStaticHostInput{
-		Name:       plan.Name.ValueString(),
-		Ip:         plan.Ip.ValueString(),
+	// setting input
+	input := cato_models.UpdateStaticHostInput{
+		Name:       plan.Name.ValueStringPointer(),
+		IP:         plan.Ip.ValueStringPointer(),
 		MacAddress: plan.MacAddress.ValueStringPointer(),
 	}
 
-	_, err := r.client.UpdateStaticHost(plan.SiteId.ValueString(), plan.Id.ValueString(), input)
+	tflog.Debug(ctx, "static_host update", map[string]interface{}{
+		"input": utils.InterfaceToJSONString(input),
+	})
+
+	_, err := r.client.catosdk.SiteUpdateStaticHost(ctx, plan.Id.ValueString(), input, r.client.AccountId)
 	if err != nil {
 		resp.Diagnostics.AddError(
-			"Cato API error",
+			"Catov2 API error",
 			err.Error(),
 		)
 		return
@@ -154,24 +171,38 @@ func (r *staticHostResource) Delete(ctx context.Context, req resource.DeleteRequ
 		return
 	}
 
-	// check if site exist before removing
-	siteExists, err := r.client.SiteExists(state.SiteId.ValueString())
+	querySiteResult, err := r.client.catosdk.EntityLookup(ctx, r.client.AccountId, cato_models.EntityType("site"), nil, nil, nil, nil, []string{state.SiteId.ValueString()}, nil, nil, nil)
 	if err != nil {
 		resp.Diagnostics.AddError(
-			"Unable to connect or request the Cato API",
+			"Catov2 API error",
 			err.Error(),
 		)
 		return
 	}
 
-	if siteExists {
-		_, err := r.client.RemoveStaticHost(state.SiteId.ValueString(), state.Id.ValueString())
+	// check if site exist before removing
+	if len(querySiteResult.EntityLookup.GetItems()) == 1 {
+
+		queryHostResult, err := r.client.catosdk.EntityLookup(ctx, r.client.AccountId, cato_models.EntityType("host"), nil, nil, nil, nil, []string{state.Id.ValueString()}, nil, nil, nil)
 		if err != nil {
 			resp.Diagnostics.AddError(
-				"Unable to connect or request the Cato API",
+				"Catov2 API error",
 				err.Error(),
 			)
 			return
+		}
+
+		// check if host exist before removing
+		if len(queryHostResult.EntityLookup.GetItems()) == 1 {
+
+			_, err := r.client.catosdk.SiteRemoveStaticHost(ctx, state.Id.ValueString(), r.client.AccountId)
+			if err != nil {
+				resp.Diagnostics.AddError(
+					"Catov2 API error",
+					err.Error(),
+				)
+				return
+			}
 		}
 	}
 
