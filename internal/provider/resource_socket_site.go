6455089e82dce60d95f0010b99a5f12592c5482d
Benjamin SIMON <benjamin.simon@catonetworks.com>
2024-09-17 18:41:04 +0200

    migrate socket_site resource to new cato sdk

---
+++ internal/provider/resource_socket_site.go
@@ -0,0 +1,429 @@
+package provider
+
+import (
+	"context"
+	"strings"
+
+	"github.com/BenEkpy/terraform-provider-cato-oss/internal/utils"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	cato_go_sdk "github.com/routebyintuition/cato-go-sdk"
+	cato_models "github.com/routebyintuition/cato-go-sdk/models"
+)
+
+var (
+	_ resource.Resource              = &socketSiteResource{}
+	_ resource.ResourceWithConfigure = &socketSiteResource{}
+)
+
+func NewSocketSiteResource() resource.Resource {
+	return &socketSiteResource{}
+}
+
+type socketSiteResource struct {
+	client *catoClientData
+}
+
+func (r *socketSiteResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_socket_site"
+}
+
+func (r *socketSiteResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description: "Identifier for the site",
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"name": schema.StringAttribute{
+				Description: "Site name",
+				Required:    true,
+			},
+			"connection_type": schema.StringAttribute{
+				Description: "Connection type for the site (SOCKET_X1500, SOCKET_AWS1500, SOCKET_AZ1500, ...)",
+				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"site_type": schema.StringAttribute{
+				Description: "Site type (BRANCH, DATACENTER, ...)",
+				Required:    true,
+			},
+			"description": schema.StringAttribute{
+				Description: "Site description",
+				Optional:    true,
+			},
+			"native_range": schema.SingleNestedAttribute{
+				Description: "Site native range settings",
+				Required:    true,
+				Attributes: map[string]schema.Attribute{
+					"native_network_range": schema.StringAttribute{
+						Description: "Site native IP range (CIDR)",
+						Required:    true,
+					},
+					"native_network_range_id": schema.StringAttribute{
+						Description: "Site native IP range ID (for update purpose)",
+						Optional:    true,
+						Computed:    true,
+						PlanModifiers: []planmodifier.String{
+							stringplanmodifier.UseStateForUnknown(),
+						},
+					},
+					"local_ip": schema.StringAttribute{
+						Description: "Site native range local ip",
+						Required:    true,
+					},
+					"translated_subnet": schema.StringAttribute{
+						Description: "Site translated native IP range (CIDR)",
+						Optional:    true,
+					},
+					"dhcp_settings": schema.SingleNestedAttribute{
+						Description: "Site native range DHCP settings",
+						Optional:    true,
+						Attributes: map[string]schema.Attribute{
+							"dhcp_type": schema.StringAttribute{
+								Description: "Site native range dhcp type",
+								Required:    true,
+							},
+							"ip_range": schema.StringAttribute{
+								Description: "Site native range dhcp range",
+								Optional:    true,
+							},
+							"relay_group_id": schema.StringAttribute{
+								Description: "Site native range dhcp relay group id",
+								Optional:    true,
+							},
+						},
+					},
+				},
+			},
+			"site_location": schema.SingleNestedAttribute{
+				Description: "Site location",
+				Required:    true,
+				Attributes: map[string]schema.Attribute{
+					"country_code": schema.StringAttribute{
+						Description: "Site country code (can be retrieve from entityLookup)",
+						Required:    true,
+					},
+					"state_code": schema.StringAttribute{
+						Description: "Optionnal site state code(can be retrieve from entityLookup)",
+						Optional:    true,
+					},
+					"timezone": schema.StringAttribute{
+						Description: "Site timezone (can be retrieve from entityLookup)",
+						Required:    true,
+					},
+					// "city": schema.StringAttribute{
+					// 	Description: "Optionnal city",
+					// 	Optional:    true,
+					// },
+					"address": schema.StringAttribute{
+						Description: "Optionnal address",
+						Optional:    true,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (r *socketSiteResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	r.client = req.ProviderData.(*catoClientData)
+}
+
+func (r *socketSiteResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+
+	var plan SocketSite
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// setting input & input to update network range
+	input := cato_models.AddSocketSiteInput{}
+	inputUpdateNetworkRange := cato_models.UpdateNetworkRangeInput{}
+
+	// setting input site location
+	if !plan.SiteLocation.IsNull() {
+		input.SiteLocation = &cato_models.AddSiteLocationInput{}
+		siteLocationInput := SiteLocation{}
+		diags = plan.SiteLocation.As(ctx, &siteLocationInput, basetypes.ObjectAsOptions{})
+		resp.Diagnostics.Append(diags...)
+
+		input.SiteLocation.Address = siteLocationInput.Address.ValueStringPointer()
+		// input.SiteLocation.City = siteLocationInput.City.ValueStringPointer()
+		input.SiteLocation.CountryCode = siteLocationInput.CountryCode.ValueString()
+		input.SiteLocation.StateCode = siteLocationInput.StateCode.ValueStringPointer()
+		input.SiteLocation.Timezone = siteLocationInput.Timezone.ValueString()
+	}
+
+	// setting input native range
+	if !plan.NativeRange.IsNull() {
+		nativeRangeInput := NativeRange{}
+		diags = plan.NativeRange.As(ctx, &nativeRangeInput, basetypes.ObjectAsOptions{})
+		resp.Diagnostics.Append(diags...)
+
+		input.NativeNetworkRange = nativeRangeInput.NativeNetworkRange.ValueString()
+		input.TranslatedSubnet = nativeRangeInput.TranslatedSubnet.ValueStringPointer()
+
+		inputUpdateNetworkRange.Subnet = nativeRangeInput.NativeNetworkRange.ValueStringPointer()
+		inputUpdateNetworkRange.TranslatedSubnet = nativeRangeInput.TranslatedSubnet.ValueStringPointer()
+		inputUpdateNetworkRange.LocalIP = nativeRangeInput.LocalIp.ValueStringPointer()
+
+		// setting input native range DHCP settings
+		if !nativeRangeInput.DhcpSettings.IsNull() {
+			inputUpdateNetworkRange.DhcpSettings = &cato_models.NetworkDhcpSettingsInput{}
+			dhcpSettingsInput := DhcpSettings{}
+			diags = nativeRangeInput.DhcpSettings.As(ctx, &dhcpSettingsInput, basetypes.ObjectAsOptions{})
+			resp.Diagnostics.Append(diags...)
+
+			inputUpdateNetworkRange.DhcpSettings.DhcpType = (cato_models.DhcpType)(dhcpSettingsInput.DhcpType.ValueString())
+			inputUpdateNetworkRange.DhcpSettings.IPRange = dhcpSettingsInput.IpRange.ValueStringPointer()
+			inputUpdateNetworkRange.DhcpSettings.RelayGroupID = dhcpSettingsInput.RelayGroupId.ValueStringPointer()
+		}
+	}
+
+	// setting input other attributes
+	input.Name = plan.Name.ValueString()
+	input.ConnectionType = (cato_models.SiteConnectionTypeEnum)(plan.ConnectionType.ValueString())
+	input.SiteType = (cato_models.SiteType)(plan.SiteType.ValueString())
+	input.Description = plan.Description.ValueStringPointer()
+
+	tflog.Debug(ctx, "socketSite create", map[string]interface{}{
+		"input-socketSite":   utils.InterfaceToJSONString(input),
+		"input-networkRange": utils.InterfaceToJSONString(inputUpdateNetworkRange),
+	})
+
+	socketSite, err := r.client.catov2.SiteAddSocketSite(ctx, input, r.client.AccountId)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Catov2 API SiteAddSocketSite error",
+			err.Error(),
+		)
+		return
+	}
+
+	// retrieving native-network range ID to update native range
+	entityParent := cato_models.EntityInput{
+		ID:   socketSite.Site.AddSocketSite.GetSiteID(),
+		Type: (cato_models.EntityType)("site"),
+	}
+
+	siteRangeEntities, err := r.client.catov2.EntityLookup(ctx, r.client.AccountId, cato_models.EntityType("siteRange"), nil, nil, &entityParent, nil, nil, nil, nil, nil)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Catov2 API EntityLookup error",
+			err.Error(),
+		)
+		return
+	}
+
+	var networkRangeEntity cato_go_sdk.EntityLookup_EntityLookup_Items_Entity
+	for _, item := range siteRangeEntities.EntityLookup.Items {
+		splitName := strings.Split(*item.Entity.Name, " \\ ")
+		if splitName[2] == "Native Range" {
+			networkRangeEntity = item.Entity
+		}
+	}
+
+	_, err = r.client.catov2.SiteUpdateNetworkRange(ctx, networkRangeEntity.GetID(), inputUpdateNetworkRange, r.client.AccountId)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Catov2 API SiteUpdateNetworkRange error",
+			err.Error(),
+		)
+		return
+	}
+
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// overiding state with socket site id
+	resp.State.SetAttribute(ctx, path.Empty().AtName("id"), types.StringValue(socketSite.Site.AddSocketSite.GetSiteID()))
+	// overiding state with native network range id
+	resp.State.SetAttribute(ctx, path.Root("native_range").AtName("native_network_range_id"), networkRangeEntity.ID)
+}
+
+func (r *socketSiteResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+
+	var state SocketSite
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// check if site exist, else remove resource
+	querySiteResult, err := r.client.catov2.EntityLookup(ctx, r.client.AccountId, cato_models.EntityType("site"), nil, nil, nil, nil, []string{state.Id.ValueString()}, nil, nil, nil)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Catov2 API error",
+			err.Error(),
+		)
+		return
+	}
+
+	// check if site exist before refreshing
+	if len(querySiteResult.EntityLookup.GetItems()) != 1 {
+		tflog.Warn(ctx, "site not found, site resource removed")
+		resp.State.RemoveResource(ctx)
+		return
+	}
+
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *socketSiteResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+
+	var plan SocketSite
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// setting input & input to update network range
+	inputSiteGeneral := cato_models.UpdateSiteGeneralDetailsInput{
+		SiteLocation: &cato_models.UpdateSiteLocationInput{},
+	}
+
+	inputUpdateNetworkRange := cato_models.UpdateNetworkRangeInput{
+		DhcpSettings: &cato_models.NetworkDhcpSettingsInput{
+			DhcpType: (cato_models.DhcpType)("DHCP_DISABLED"),
+		},
+	}
+
+	// setting input site location
+	if !plan.SiteLocation.IsNull() {
+		inputSiteGeneral.SiteLocation = &cato_models.UpdateSiteLocationInput{}
+		siteLocationInput := SiteLocation{}
+		diags = plan.SiteLocation.As(ctx, &siteLocationInput, basetypes.ObjectAsOptions{})
+		resp.Diagnostics.Append(diags...)
+
+		inputSiteGeneral.SiteLocation.Address = siteLocationInput.Address.ValueStringPointer()
+		inputSiteGeneral.SiteLocation.CountryCode = siteLocationInput.CountryCode.ValueStringPointer()
+		inputSiteGeneral.SiteLocation.StateCode = siteLocationInput.StateCode.ValueStringPointer()
+		inputSiteGeneral.SiteLocation.Timezone = siteLocationInput.Timezone.ValueStringPointer()
+	}
+
+	// setting input native range
+	if !plan.NativeRange.IsNull() {
+		nativeRangeInput := NativeRange{}
+		diags = plan.NativeRange.As(ctx, &nativeRangeInput, basetypes.ObjectAsOptions{})
+		resp.Diagnostics.Append(diags...)
+
+		inputUpdateNetworkRange.TranslatedSubnet = nativeRangeInput.TranslatedSubnet.ValueStringPointer()
+		inputUpdateNetworkRange.Subnet = nativeRangeInput.NativeNetworkRange.ValueStringPointer()
+		inputUpdateNetworkRange.TranslatedSubnet = nativeRangeInput.TranslatedSubnet.ValueStringPointer()
+		inputUpdateNetworkRange.LocalIP = nativeRangeInput.LocalIp.ValueStringPointer()
+
+		// setting input native range DHCP settings
+		if !nativeRangeInput.DhcpSettings.IsNull() {
+			inputUpdateNetworkRange.DhcpSettings = &cato_models.NetworkDhcpSettingsInput{}
+			dhcpSettingsInput := DhcpSettings{}
+			diags = nativeRangeInput.DhcpSettings.As(ctx, &dhcpSettingsInput, basetypes.ObjectAsOptions{})
+			resp.Diagnostics.Append(diags...)
+
+			inputUpdateNetworkRange.DhcpSettings.DhcpType = (cato_models.DhcpType)(dhcpSettingsInput.DhcpType.ValueString())
+			inputUpdateNetworkRange.DhcpSettings.IPRange = dhcpSettingsInput.IpRange.ValueStringPointer()
+			inputUpdateNetworkRange.DhcpSettings.RelayGroupID = dhcpSettingsInput.RelayGroupId.ValueStringPointer()
+		}
+	}
+
+	// setting input other attributes
+	inputUpdateNetworkRange.Name = plan.Name.ValueStringPointer()
+	inputSiteGeneral.SiteType = (*cato_models.SiteType)(plan.SiteType.ValueStringPointer())
+	inputSiteGeneral.Description = plan.Description.ValueStringPointer()
+
+	tflog.Debug(ctx, "socketSite update", map[string]interface{}{
+		"input-socketSite":   utils.InterfaceToJSONString(inputSiteGeneral),
+		"input-networkRange": utils.InterfaceToJSONString(inputUpdateNetworkRange),
+	})
+
+	_, err := r.client.catov2.SiteUpdateSiteGeneralDetails(ctx, plan.Id.ValueString(), inputSiteGeneral, r.client.AccountId)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Catov2 API SiteUpdateSiteGeneralDetails error",
+			err.Error(),
+		)
+		return
+	}
+
+	//retrieve native range ID
+	nativeRange := NativeRange{}
+	diags = plan.NativeRange.As(ctx, &nativeRange, basetypes.ObjectAsOptions{})
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	_, err = r.client.catov2.SiteUpdateNetworkRange(ctx, nativeRange.NativeNetworkRangeId.ValueString(), inputUpdateNetworkRange, r.client.AccountId)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Catov2 API SiteUpdateNetworkRange error",
+			err.Error(),
+		)
+		return
+	}
+
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+}
+
+func (r *socketSiteResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+
+	var state SocketSite
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	querySiteResult, err := r.client.catov2.EntityLookup(ctx, r.client.AccountId, cato_models.EntityType("site"), nil, nil, nil, nil, []string{state.Id.ValueString()}, nil, nil, nil)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Catov2 API error",
+			err.Error(),
+		)
+		return
+	}
+
+	// check if site exist before removing
+	if len(querySiteResult.EntityLookup.GetItems()) == 1 {
+
+		_, err := r.client.catov2.SiteRemoveSite(ctx, state.Id.ValueString(), r.client.AccountId)
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Catov2 API SiteRemoveSite error",
+				err.Error(),
+			)
+			return
+		}
+	}
+}
